774f7c97dfba77d93aa34ffd73cceadcf680fc09
vinhlikeit <phamhoavinhvinh31@gmail.com>
2020-07-06 04:05:26 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -2,7 +2,78 @@
 // This part belongs to creating events
 const API_KEY = 'AIzaSyD1Xyr_2_aX3Qr7w-SqfYwfXighY0Ngnb0'
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+var beginDate = ''
+var endDate = ''
+var successfulRequest = 0
+var classInformation = []
+var inputInformation = {}
+// This function converts the time extracted from my.Whitman to
+// a string that Google Calendar can understand
 
+function convertTime(time) {
+
+  const indicator = time.slice(-2)
+
+  if (indicator == 'PM') {
+
+    var hour = parseInt(time.slice(0, 2))
+
+    if (hour != 12) {
+      hour += 12
+    }
+
+    time = time.replace(time.slice(0, 2), hour.toString())
+
+  }
+  return time.slice(0, 5) + ":00"
+}
+
+// This function converts the days extracted from my.Whitman to
+// a string that Google Calendar can understand
+function dayConvert(day) {
+  var result = ""
+
+  for (var index = 0; index < day.length; index++) {
+
+    var initial = ""
+
+    if (day[index + 1] == 'H') {
+      initial = "TH"
+      index++
+    } else {
+      switch (day[index]) {
+        case "M":
+          initial = "MO"
+          break
+        case "T":
+          initial = "TU"
+          break
+        case "W":
+          initial = "WE"
+          break
+        case "F":
+          initial = "FR"
+          break
+        case "S":
+          initial = "SU"
+      }
+    }
+
+    result = result.concat(',' + initial)
+  }
+  return result.replace(result[0], '')
+}
+
+// This function converts a number less than 10 to a string
+// with adding an additional zero
+function addValueBelowTwoDigits(value) {
+  var returnString = ''
+  if (value < 10) {
+    returnString += '0'
+  }
+  returnString += value
+  return returnString
+}
 
 // Load GAPI
 function onGAPILoad() {
@@ -12,42 +83,158 @@ function onGAPILoad() {
   })
 }
 
+function convert(course, reminder) {
+  // Initalize necessary constants
+  const startTime = convertTime(course.details.timeLocation.startTime)
+  const endTime = convertTime(course.details.timeLocation.endTime)
+  const classDays = dayConvert(course.details.timeLocation.days)
+  const startDate = (function() {
+    const aDayInMilliSec = 86400000
+    const days = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"]
+    var date = new Date(beginDate.slice(0, 4), beginDate.slice(5, 7) - 1, beginDate.slice(8, 10))
+
+    while (days[date.getDay()] != classDays.slice(0, 2)) {
+      date = new Date(date.getTime() + aDayInMilliSec)
+    }
+
+    var result = date.getUTCFullYear() + '-'
+    result += addValueBelowTwoDigits(date.getMonth() + 1) + '-'
+    result += addValueBelowTwoDigits(date.getDate())
+    return result
+  })
+  const stopDate = endDate.replace(/-/g, '')
+  const timeZone = 'America/Los_Angeles'
+  const description = course.details.subName + '\n' + course.details.professor
+  const setReminder = (function() {
+    result = {
+      useDefault : true
+    }
+    if (reminder.beforeClass != "") {
+
+      result = {
+        overrides: [{}]
+      }
+      result.useDefault = false
+
+      if (reminder.betweenClass != "") {
+
+      } else {
+
+        result.overrides[0].method = reminder.options
+        result.overrides[0].minutes = parseInt(reminder.beforeClass) * 60
+
+      }
+    }
+    console.log(result)
+    return result
+  })
+  const googleCalendarEvent = {
+    "summary": course.details.name,
+    "location": course.details.timeLocation.building + " " + course.details.timeLocation.room,
+    "start": {
+      "dateTime": startDate() + "T" + startTime + "-07:00",
+      "timeZone": timeZone
+    },
+    "end": {
+      "dateTime": startDate() + "T" + endTime + "-07:00",
+      "timeZone": timeZone
+    },
+    "recurrence": [
+      "RRULE:FREQ=WEEKLY;UNTIL=" + stopDate + ";BYDAY=" + classDays
+    ],
+    "colorId": course.details.color,
+    "description": description,
+    "reminders": setReminder()
+  }
+
+  return googleCalendarEvent
+
+}
+
+function initalizeToken() {
+  chrome.identity.getAuthToken({
+    interactive: true
+  }, function(token) {
+    // Set GAPI auth token
+    gapi.auth.setToken({
+      'access_token': token,
+    });
+  })
+}
+
+function setReminder(googleCalendarEvent, reminderTime) {
+  console.log(googleCalendarEvent)
+  googleCalendarEvent.reminder.useDefault = true
+
+}
+
+function createEventsOnGoogleCalendar(listOfClasses, calendar, reminder) {
+
+  listOfClasses.sort(function (classA, classB) {
+    const timeA  = convertTime(classA.details.timeLocation.startTime)
+    const timeB  = convertTime(classB.details.timeLocation.startTime)
+    if (timeA < timeB){
+      return -1
+    } else {return 1}
+  })
+  console.log(listOfClasses)
+  const numberOfClasses = listOfClasses.length
+
+  // Classes loaded successfully
+
+  // Creating events
+  for (var index = 0; index < numberOfClasses; index++) {
+
+    // Initalize
+    // CurrentClass is the class the API executing
+    const currentClass = convert(listOfClasses[index], reminder)
+    // Initalize google calendar event
+    const request = gapi.client.calendar.events.insert({
+      'calendarId': calendar,
+      'resource': currentClass
+    })
+
+    // Excute events
+    request.execute(function(event) {
+      if (event.hasOwnProperty('code')) {
+        alert(' Error')
+      }
+      console.log(event)
+    })
+
+  }
+
+}
 
 // Waiting the message from content script
 // If receive the message, execute request
 // creating events on google calendar
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  chrome.identity.getAuthToken({interactive: true}, function(token) {
-          // Authorization
-          gapi.auth.setToken({
-            'access_token': token
-          })
+  initalizeToken()
 
-          const classes = request.Message
-          const numberOfClasses = classes.length
-          var successfulRequest = 0
-          // Classes loaded successfully
+  switch (request.signal) {
+    case 'sendInput':
+      inputInformation = request
+      break;
+    case 'createEvent':
+      console.log('%c request:', 'color:orange; font-weigth:bold', inputInformation)
+      beginDate = inputInformation.time.startDate
+      endDate = inputInformation.time.endDate
+      createEventsOnGoogleCalendar(request.classes, inputInformation.calendar, inputInformation.reminder)
+      break;
 
-          // Creating events
-          for (var index = 0; index < numberOfClasses; index++) {
+    case 'getCalendarList':
+      var request = gapi.client.calendar.calendarList.list();
 
-            // Initalize
-            // CurrentClass is the class the API executing
-            const currentClass = classes[index]
-            // Initalize google calendar event
-            const request = gapi.client.calendar.events.insert({
-              'calendarId': 'primary',
-              'resource': currentClass
-            })
-
-            // Excute events
-            request.execute(function(event) {
-              if (event.status = "confirmed") {
-                successfulRequest += 1
-              }
-              console.log(event)
-            })
-
-          }
+      request.execute(function(response) {
+        console.log(response.items)
+        chrome.runtime.sendMessage({
+          signal: 'loadCalendarList',
+          calendars: response.items
         })
+      })
+      break;
+    default:
+      break;
+  }
 })
--- popup.js
+++ popup.js
@@ -1,23 +1,96 @@
 // Send messages to content.js whenever a button is clicled
-function popupDidLoad() {
+const startDateInput = document.getElementById('sDate')
+const endDateInput = document.getElementById('eDate')
+var calendarIdList = []
+var select = document.getElementById('calendarOption')
+var message = {}
+
+function loadTermAndDate() {
   var title = document.getElementById("titleText")
+  const selectedTerm = ""
+  chrome.tabs.query({
+      currentWindow: true,
+      active: true
+    },
+    function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        signal: 'getTerm'
+      }, function(respone) {
+        const year = respone.slice(0, 4)
+        const term = respone.slice(-2)
+        switch (term) {
+          case 'FA':
+            title.textContent = term + 'LL ' + year
+            startDateInput.value = year + '-08-30'
+            endDateInput.value = year + '-12-10'
+            break;
+          case 'SP':
+            title.textContent = term + 'RING ' + year
+            startDateInput.value = year + '-01-18'
+            endDateInput.value = year + '-05-08'
+            break;
+          default:
+            break;
+        }
+
+      })
+    }
+  )
+}
+
+function loadCalendarList(){
+  chrome.runtime.sendMessage({
+    signal: 'getCalendarList'
+  })
+  chrome.runtime.onMessage.addListener(function(request){
+    if (request.signal == 'loadCalendarList') {
+      console.log(request)
+
+      for (var calendar of request.calendars) {
+        var option = document.createElement('Option')
+        if (calendar.hasOwnProperty('primary')) {
+          option.text = 'Primary'
+          calendarIdList.push('primary')
+        } else {
+          option.text = calendar.summary
+          calendarIdList.push(calendar.id)
+        }
+        select.add(option)
+      }
+    }
+
+  })
+
+}
+
+function popupDidLoad() {
+  loadTermAndDate()
+  loadCalendarList()
 }
 
 function clickAction(e) {
-
+  console.log(e)
   chrome.tabs.query({
       currentWindow: true,
       active: true
     },
     function(tabs) {
       message = {
+        signal: "sendInput",
         time: {
-          startDate: document.getElementById('sDate').value,
-          endDate: document.getElementById('eDate').value
+          startDate: startDateInput.value,
+          endDate: endDateInput.value
+        },
+        calendar: calendarIdList[select.selectedIndex],
+        reminder: {
+          beforeClass: reminderTime.value,
+          betweenClass: reminderBetweenTime.value,
+          options: notificationOption.options[notificationOption.selectedIndex].value
         }
-
       }
-      chrome.tabs.sendMessage(tabs[0].id, message)
+      chrome.tabs.sendMessage(tabs[0].id, {signal: "callToAction"})
+      chrome.runtime.sendMessage(message)
+      console.log(message)
     })
 }
 
@@ -25,8 +98,9 @@ function clickAction(e) {
 // Listen for the user clicking the button
 // Execute request
 
-popupDidLoad()
+
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('createButton').addEventListener('click', clickAction);
+  popupDidLoad()
+  document.getElementById('createButton').onclick = clickAction
 });
